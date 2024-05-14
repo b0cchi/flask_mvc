55ee52d1ee80dd7f82bddc3c46500ed38ee318f1
b0cchi <b0cchi.vv@gmail.com>
2024-05-14 17:42:42 +0900

    change css

--- workspace/app.py
+++ workspace/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def index():
-    title = 'hello!!'
+    title = 'Flask MVC'
     return render_template('index.html',title = title)
 
 if __name__ == '__main__':
